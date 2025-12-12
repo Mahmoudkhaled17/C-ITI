86e84f85550b707355c0175cb8a81a351164a5d0
Mahmoud khaled <164625524+Mahmoudkhaled17@users.noreply.github.com>
2025-12-13 01:10:35 +0200

    Add files via upload

---
+++ Magic box/Magic box/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Magic_box
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // magic box
+            Console.WriteLine("Enter the number ");
+            string Raw = Console.ReadLine();
+            int row = int.Parse(Raw);
+
+            int col = row;
+            int cells = row * col;
+
+
+
+            int row_t1 = 0
+                , col_t1 = 0;
+
+            row_t1 = (row / 2);
+            col_t1 = (col / 2) + 1;
+
+
+            Console.WriteLine("");for (int i = 1; i <= cells; i++)
+            {
+                    Console.SetCursorPosition((col_t1)*(120/(row+2)), (row_t1)*(30/ (row + 2)));
+                    Console.WriteLine(i);
+
+                    
+                    int detect = i %col;
+
+
+                    if (detect!=0) 
+                    {
+                        col_t1 = col_t1-1;
+                        row_t1 = row_t1-1;
+
+                        if(row_t1==0)
+                        {
+                            row_t1 = row;
+                         
+                        }
+                        if (col_t1==0)
+                        {
+                            col_t1 = col;
+                        }
+                    }
+                    else 
+                    {
+                        
+                        row_t1 = row_t1 +1;
+                    
+                        
+                    }
+
+
+
+                
+                    }
+             
+                
+
+
+
+                
+            }
+        }
+    }
+
---
+++ simple-menui/simple-menui/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace simple_menui
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first number");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("second number");
+            string secondInput = Console.ReadLine();
+            int firstNumber = int.Parse(firstInput);
+            int secondNumber = int.Parse(secondInput);
+
+            Console.WriteLine("Choose an operation: 1. +\n 2.- \n 3.* \n 4./");
+            string operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "1":
+                    Console.WriteLine($"Result: {firstNumber + secondNumber}");
+                    break;
+                case "2":
+                    Console.WriteLine($"Result: {firstNumber - secondNumber}");
+                    break;
+                case "3":
+                    Console.WriteLine($"Result: {firstNumber * secondNumber}");
+                    break;
+                case "4":
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                    }
+                    else
+                    {
+                        float divResult = (float)firstNumber / secondNumber;
+                        Console.WriteLine($"Result: {divResult}");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation selected.");
+                    break;
+            }
+        }
+    }
+}
