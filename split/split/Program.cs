567fcc516a9e664d9a59eec42f13a6a2c85cc018
Mahmoud khaled <164625524+Mahmoudkhaled17@users.noreply.github.com>
2025-12-13 01:09:46 +0200

    Add files via upload

---
+++ split/split/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter expersion");
+            string input = Console.ReadLine();
+            string[] parts = input.Split(new char[] { '+', '-', '*', '/' });
+
+            int index = input.IndexOfAny(new[] { '+', '-', '*', '/' });
+
+            Console.WriteLine($"First part: {parts[0]}");
+            Console.WriteLine($"Operator: {input[index]}");
+            Console.WriteLine($"Second part: {parts[1]}");
+
+           switch(input[index])
+            {
+                case '+':
+                    Console.WriteLine($"Result: {int.Parse(parts[0]) + int.Parse(parts[1])}");
+                    break;
+                case '-':
+                    Console.WriteLine($"Result: {int.Parse(parts[0]) - int.Parse(parts[1])}");
+                    break;
+                case '*':
+                    Console.WriteLine($"Result: {int.Parse(parts[0]) * int.Parse(parts[1])}");
+                    break;
+                case '/':
+                    Console.WriteLine($"Result: {int.Parse(parts[0]) / int.Parse(parts[1])}");
+                    break;
+                default:
+                    Console.WriteLine("Unknown operator");
+                    break;
+            }
+        }
+    }
+}
---
+++ with-myself/with-myself/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace with_myself
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Value ? ");
+            string x=Console.ReadLine();
+            Console.WriteLine("The Value is : " + x);
+            string y = (x == "10") ? "ten" : "not ten";
+            Console.WriteLine("The Value in words is : " + y);
+            int[]arr={1,2,3,4,5};
+
+            Console.WriteLine("Enter the size of array ?");
+            int num=int.Parse(Console.ReadLine());
+            int[]arr2=new int[num];
+            for(int i=0;i<num;i++)
+            {
+                Console.WriteLine("Enter the value for index "+i);
+                arr2[i]=int.Parse(Console.ReadLine());
+            }
+
+            for(int i=0;i<num;i++)
+            {
+                Console.Write(arr2[i]+" ");
+            }
+
+            Console.WriteLine("\n enter the array2");
+            string nn=Console.ReadLine();
+            string[]arr3=nn.Split(',');
+            foreach(string s in arr3)
+            {
+                Console.WriteLine(s);
+            }
+
+           
+
+        }
+    }
+}
