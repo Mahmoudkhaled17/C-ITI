9bde6d9b21c5f1b074435ddc502dc4262278469c
Mahmoud khaled <164625524+Mahmoudkhaled17@users.noreply.github.com>
2025-12-13 01:25:10 +0200

    Add files via upload

---
+++ day6/day6/Program.cs
@@ -0,0 +1,101 @@
+﻿using employee;
+namespace day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] menu = { "new", "display", "exit" };
+            int choice = 0;
+            //Employee employee = new Employee();
+            class1 emp = new class1();
+            bool loop = true;
+            do
+            {
+                for (int i = 0; i < menu.Length; i++)
+                {
+                    if (i == choice)
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                    else
+                        Console.ResetColor();
+
+                    Console.SetCursorPosition(120 / 2, (30 / 4) * (i + 1));
+                    Console.WriteLine(menu[i]);
+                }
+                ConsoleKeyInfo k = Console.ReadKey();
+                switch (k.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        choice--;
+                        if (choice < 0)
+                            choice = menu.Length - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        choice++;
+                        if (choice >= menu.Length)
+                            choice = 0;
+                        break;
+                    case ConsoleKey.Enter:
+
+                        if (choice == 0)
+                        {
+                            Console.Clear();
+                            //employee.get();
+                            get(emp);
+
+                            Console.Clear();
+
+                        }
+                        if (choice == 1)
+                        {
+                            Console.Clear();
+
+                            //employee.show();
+                            display(emp);
+
+                            Console.ReadKey();
+                            Console.Clear();
+
+
+
+                        }
+                        if (choice == 2)
+                        {
+                            Console.Clear();
+                            loop = false;
+                            break;
+
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.Clear();
+                        loop = false;
+                        break;
+
+                }
+            } while (loop);
+
+        }
+        public static void get(class1 obj)
+        {
+            Console.WriteLine("Enter Name:");
+            obj.name2 = Console.ReadLine();
+            Console.WriteLine("Enter id:");
+            obj.id2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Salary:");
+            obj.Salary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter age:");
+            obj.age2 = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+        }
+        public static void display(class1 obj)
+        {
+            Console.WriteLine("Name: " + obj.name2);
+            Console.WriteLine("ID: " + obj.id2);
+            Console.WriteLine("Salary: " + obj.Salary);
+            Console.WriteLine("Age: " + obj.age2);
+
+        }
+    }
+}
---
+++ day6/employee/Class1.cs
@@ -0,0 +1,80 @@
+﻿namespace employee
+{
+    public enum Gender
+    {
+        Male, Female
+    }
+    public class Employee
+    {
+        public string name;
+        public int id;
+        public double salary;
+        public int age;
+        public Gender gender;
+
+        public Employee()
+        {
+            name = "unknown";
+            id = 0;
+            salary = 0.0;
+            age = 0;
+            gender = Gender.Male;
+        }
+
+
+        public void get()
+        {
+            Console.Write("Enter Name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter ID: ");
+            id = int.Parse(Console.ReadLine());
+            Console.Write("Enter Salary: ");
+            salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter age");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("write the gender");
+            string genderInput = Console.ReadLine();
+            if (genderInput == "m")
+            {
+                gender = Gender.Male;
+            }
+            else if (genderInput == "f")
+            {
+                gender = Gender.Female;
+            }
+        }
+        public void show()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Salary: " + salary);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("gender: " + gender);
+        }
+    }
+
+
+    public class class1
+    {
+        public string name2;
+        public int id2;
+        public double salary2;
+        public int age2;
+
+        public double Salary
+        {
+            get { return salary2; }
+            set
+            {
+                if (value < 0)
+                {
+                    salary2 = 0;
+                }
+                else
+                {
+                    salary2 = value;
+                }
+            }
+        }
+    }
+}
