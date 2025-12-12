7fc6854e0f6177db167d3d074e80cc96e3837467
Mahmoud khaled <164625524+Mahmoudkhaled17@users.noreply.github.com>
2025-12-13 01:08:36 +0200

    Add files via upload

---
+++ get max between two/get max between two/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace get_max_between_two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first number");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("second number");
+            string input2 = Console.ReadLine();
+            int number1 = int.Parse(input1);
+            int number2 = int.Parse(input2);
+
+            if (number1 > number2)
+            {
+                Console.WriteLine("The maximum number is: " + number1);
+            }
+            else
+            {
+                Console.WriteLine("The maximum number is: " + number2);
+            }
+        }
+    }
+}
